8c13da179cdb2e8a1c44ef83a5d07aa6f15ad46c
Christopher Ngo <ingochris@users.noreply.github.com>
2017-09-17 05:32:57 -0700

    FIX: rm 'pts = deque(maxlen=args["buffer"])'

--- person_tracking.py
+++ person_tracking.py
@@ -11,7 +11,6 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
-pts = deque(maxlen=args["buffer"])
 
 person_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 # face_cascade = cv2.CascadeClassifier('haarcascade_face.xml')
